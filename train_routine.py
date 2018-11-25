81694366835453801c51ff790767afed13ba29fa
Denis Filippov <dfilippov@oneincsystems.com>
2018-11-25 21:23:13 +0700

    fix train

--- train_routine.py
+++ train_routine.py
@@ -1,3 +1,5 @@
-def fit(epoch, device, model, data_loader, loss_function, optimizer, num_epoch, phase='training', report_every=20):
-    return epoch
+def fit(num_epoch, device, model, dataset, batch_generator, loss_function, optimizer, phase='training', report_every=20):
+    for epoch_index in range(num_epoch):
+        print(epoch_index)
+
 
