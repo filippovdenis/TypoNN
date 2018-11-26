00012e19b874f9470614aa4cf0b85f391116dca4
Denis Filippov <dfilippov@oneincsystems.com>
2018-11-26 22:53:53 +0700

    Added NamesDataset.py

---
+++ NamesDataset.py
@@ -0,0 +1,33 @@
+class NamesDataset(Dataset):
+    def __init__(self, data_path, alphabet, max_length):
+        self.data_path = data_path
+        self.alphabet = alphabet
+        self.max_length = max_length
+        self.label = []
+        self.data = []
+        with open(self.data_path, 'r') as f:
+            rdr = csv.reader(f, delimiter=',', quotechar='"')
+            num = 0
+            for index, fields in enumerate(rdr):
+                if num > 0:
+                    print(fields)
+                    self.data.append(fields[1])
+                    self.label.append(int(fields[3]))
+                num += 1
+        self.y = torch.LongTensor(self.label)
+    def __len__(self):
+        return len(self.label)
+    def __getitem__(self, idx):
+        X = self.one_hot(idx)
+        y = self.y[idx]
+        return X, y
+    def one_hot(self, idx):
+        X = torch.zeros(len(self.alphabet), self.max_length)
+        s = self.data[idx]
+       
+        for index_char, char in enumerate(s):
+            if self.charIndex(char) != -1:
+                X[self.charIndex(char)][index_char] = 1.0
+        return X
+    def charIndex(self, char):
+        return self.alphabet.find(char)
--- train_routine.py
+++ train_routine.py
@@ -1,9 +1,10 @@
+from torch.utils.data import DataLoader
+
 class TrainRoutine:
-    def __init__(self, device, model, dataset, batch_generator, loss_function, optimizer):
+    def __init__(self, device, model, dataset, loss_function, optimizer):
         self.device = device
         self.model = model
         self.dataset = dataset
-        self.batch_generator = batch_generator
         self.loss_function = loss_function
         self.optimizer = optimizer
 
@@ -11,6 +12,25 @@ class TrainRoutine:
     def fit(self , num_epoch):
         for epoch_index in range(num_epoch):
             print(epoch_index)
+            batch_generator = self.generate_batches(self.dataset, 128, device = self.device)
+            self.model.train()
+            for batch_index, batch_dict in enumerate(batch_generator):
+                print(batch_index)
         return self.device
+
+    def generate_batches(self, dataset, batch_size, shuffle=True,
+                        drop_last=True, device="cpu"):
+        """
+        A generator function which wraps the PyTorch DataLoader. It will 
+        ensure each tensor is on the write device location.
+        """
+        dataloader = DataLoader(dataset=dataset, batch_size=batch_size,
+                                shuffle=shuffle, drop_last=drop_last)
+
+        for data_dict in dataloader:
+            out_data_dict = {}
+            for name, tensor in data_dict.items():
+                out_data_dict[name] = data_dict[name].to(device)
+            yield out_data_dict
 
 
