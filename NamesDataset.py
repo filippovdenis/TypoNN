4ce448cb60b98b2dc753c75655c7a1d22d08e5e0
Denis Filippov <dfilippov@oneincsystems.com>
2018-11-26 23:08:14 +0700

    Fix NamesDataset.py

--- NamesDataset.py
+++ NamesDataset.py
@@ -1,6 +1,11 @@
-from torch.utils.data import Dataset
 import numpy as np
 import csv
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import string
+from torch.utils.data import DataLoader, Dataset
 
 class NamesDataset(Dataset):
     def __init__(self, data_path, alphabet, max_length):
