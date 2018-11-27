1e028d4bf20acf5427ca26c3099388f2af773296
Denis Filippov <dfilippov@oneincsystems.com>
2018-11-27 09:53:52 +0700

    fix train_routine.py

--- train_routine.py
+++ train_routine.py
@@ -12,9 +12,9 @@ class TrainRoutine:
     def fit(self , num_epoch):
         for epoch_index in range(num_epoch):
             print(epoch_index)
-            batch_generator = self.generate_batches(self.dataset, 4, device = self.device)
+            dataloader = self.generate_batches(self.dataset, 4, device = self.device)
             self.model.train()
-            for batch_index, batch_dict in enumerate(batch_generator):
+            for batch_index, batch_dict in enumerate(dataloader):
                 print(batch_index)
         return self.device
 
@@ -27,10 +27,6 @@ class TrainRoutine:
         dataloader = DataLoader(dataset=dataset, batch_size=batch_size,
                                 shuffle=shuffle, drop_last=drop_last)
 
-        for data_dict in dataloader:
-            out_data_dict = {}
-            for name, tensor in data_dict.items():
-                out_data_dict[name] = data_dict[name].to(device)
-            yield out_data_dict
+        return dataloader
 
 
